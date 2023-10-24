eec65a08da2826a2f45c9967d45e06cf80a05cd0
jjschulz <97975268+jjschulz@users.noreply.github.com>
2023-10-24 10:22:39 -0400

    Update sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,10 +1,11 @@
 a, b = 0, 1
 sum_even = 0
+#while b is less than 4 million
 while b < 4000000:
     if b % 2 == 0:
         sum_even += b
     a, b = b, a+b
-
+#print even sum
 print(sum_even)
 
 # answer should be 4613732
